0b8ce22fd2128d4561f7151628d12e1963046339
Filipe Andrade <filipe.engenharia42@gmail.com>
2022-08-02 13:55:01 -0300

    created crud of unit of measure

--- domain/tables.go
+++ domain/tables.go
@@ -2,9 +2,10 @@ package domain
 
 import "github.com/vingarcia/ksql"
 
-var UsersTable = ksql.NewTable("users", "id")
-
-// ============================================================================
-// Meu
-
-var ProductsTable = ksql.NewTable("products", "id")
+var (
+	UnitsOfMeasureTable = ksql.NewTable("units_of_measure", "id")
+	BaseProductsTable   = ksql.NewTable("base_products", "id")
+	ProductsTable       = ksql.NewTable("products", "id")
+	RolesTable          = ksql.NewTable("roles", "id")
+	UsersTable          = ksql.NewTable("users", "id")
+)
